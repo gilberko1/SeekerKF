ed5fc4d466801331b4b91f0c2c10e973c1c8dbd8
gilberko1 <67217933+gilberko1@users.noreply.github.com>
2024-07-28 21:41:02 +0300

    second commit

--- output/PlotRes.py
+++ output/PlotRes.py
@@ -4,25 +4,47 @@ import matplotlib.pyplot as plt
 # Read the CSV file
 data = pd.read_csv('kalman_output.csv')
 
-# Plot Angles vs. Time
-plt.figure(figsize=(12, 6))
+# Calculate the errors
+data['Angle Error'] = data['Correct Angle'] - data['Predicted Angle']
+data['Angle Rate Error'] = data['Correct Angle Rate'] - data['Predicted Angle Rate']
 
-plt.subplot(2, 1, 1)
+# Plot Angles vs. Time
+plt.figure(figsize=(12, 12))
+
+plt.subplot(4, 1, 1)
 plt.plot(data['Time'], data['Correct Angle'], label='Correct Angle')
 plt.plot(data['Time'], data['Predicted Angle'], label='Predicted Angle')
 plt.xlabel('Time (s)')
-plt.ylabel('Angle (degrees)')
+plt.ylabel('Angle (rad)')
 plt.title('Angles vs. Time')
 plt.legend()
 plt.grid(True)
 
 # Plot Angle Rates vs. Time
-plt.subplot(2, 1, 2)
+plt.subplot(4, 1, 2)
 plt.plot(data['Time'], data['Correct Angle Rate'], label='Correct Angle Rate')
 plt.plot(data['Time'], data['Predicted Angle Rate'], label='Predicted Angle Rate')
 plt.xlabel('Time (s)')
-plt.ylabel('Angle Rate (degrees/s)')
+plt.ylabel('Angle Rate (rad/s)')
 plt.title('Angle Rates vs. Time')
+plt.legend()
+plt.grid(True)
+
+# Plot Angle Errors vs. Time
+plt.subplot(4, 1, 3)
+plt.plot(data['Time'], data['Angle Error'], label='Angle Error')
+plt.xlabel('Time (s)')
+plt.ylabel('Angle Error (rad)')
+plt.title('Angle Error vs. Time')
+plt.legend()
+plt.grid(True)
+
+# Plot Angle Rate Errors vs. Time
+plt.subplot(4, 1, 4)
+plt.plot(data['Time'], data['Angle Rate Error'], label='Angle Rate Error')
+plt.xlabel('Time (s)')
+plt.ylabel('Angle Rate Error (rad/s)')
+plt.title('Angle Rate Error vs. Time')
 plt.legend()
 plt.grid(True)
 
